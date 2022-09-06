c8e92baa6a6d5842f6a026d642cdd08a9e6c5d76
Mu-munn <rpgmukai333@gmail.com>
2022-09-06 17:47:28 +0900

    UIの途中までです

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -1,14 +1,35 @@
-import { Flex, ButtonGroup, Button } from "@chakra-ui/react";
+import {
+  Flex,
+  ButtonGroup,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from "@chakra-ui/react"
+import LoginModal from "./utils/LoginModal"
+import { useState } from "react"
 
 function Header({ children }) {
+  const [isOp, setIsOp] = useState(false)
+  const onClick = () => {
+    setIsOp(true)
+  }
   return (
     <Flex justifyContent={"space-between"} alignItems={"center"} mb={"51px"}>
       {children}
       <ButtonGroup>
-        <Button rounded={"full"} px={6} bg="#9B9B9B" color={"white"}>
+        <Button rounded={"full"} px={6} bg='#9B9B9B' color={"white"}>
           log in
         </Button>
         <Button
+          onClick={()=>{
+            setIsOp(true)
+          }}
           rounded={"full"}
           px={6}
           colorScheme={"orange"}
@@ -18,8 +39,9 @@ function Header({ children }) {
           sign up
         </Button>
       </ButtonGroup>
+      {isOp ? <LoginModal /> : <></>}
     </Flex>
-  );
+  )
 }
 
-export default Header;
+export default Header
--- web/src/pages/homePage.jsx
+++ web/src/pages/homePage.jsx
@@ -1,5 +1,6 @@
 import { Box, SimpleGrid } from "@chakra-ui/react"
 import PostCard from "../components/posts/PostCard"
+import Header  from '../components/Header'
 
 export default function HomePage() {
   const testPost = {
@@ -29,6 +30,7 @@ export default function HomePage() {
   ]
   return (
     <Box>
+      <Header></Header>
       <SimpleGrid columns={3} bgColor='#F8F8F8' spacing={10} columnGap={2} px={20} py={10}>
         {testPosts.map((m) => {
           return <PostCard key={m.id} name={m.name} description={m.description} />
