29d54bc4218dda1f20f97b1fff394b19af084490
keigomichi <hundredacrewood1165g7@gmail.com>
2022-09-08 08:49:17 +0900

    chore: display posts in timeline page with id

--- web/src/pages/Timeline.js
+++ web/src/pages/Timeline.js
@@ -1,43 +1,32 @@
-import { Avatar, Center, VStack, Heading, Button } from "@chakra-ui/react"
-import Header from "../components/Header"
-import Post from "../components/Post"
-import { BiPlus } from "react-icons/bi"
-import { IconContext } from "react-icons"
-import { useParams, Link } from "react-router-dom"
+import { Avatar, Center, VStack, Heading, Button } from "@chakra-ui/react";
+import Header from "../components/Header";
+import Post from "../components/Post";
+import { BiPlus } from "react-icons/bi";
+import { IconContext } from "react-icons";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const PostItems = [
-  {
-    id: 1,
-    user_id: 1,
-    title: "ラーメン作りました！",
-    description: "This is the first post",
-    image_url:
-      "https://s3-alpha-sig.figma.com/img/ab88/555e/bf97db9a062b15c16368c0ad75c378de?Expires=1663545600&Signature=QwyMrfscA7V6mTNXSJCpp43kcfiBriiIhr3oZ76K0iCHh0kd8OuzcHWQRDCyObX5AYYlrwmCGi5uvZWtvArcMERrgBDRNauBwYkviJAq9ISho8eB18Ll5kEVeBCASxFKsKTy6rwEpYdIcJk36cFtUMMbOB2t9eV704157UdiPAztleDFUDTo2qRKw2sPlKszeGUDIGD6k8RJhvFCt9wOsJimY3~Xon2QQHEGRYv8PwJf3XNDvqrIFcOs5itZDa6vlA6jE1Q661pmaF0Fz4GnkVbF2o3bTJeulcEBy76OQpxy-XXAk7mCJ67TevXWV3SRSi0Yyy8WkimxuLlSlPVNCQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
-    image_alt: "image 1",
-    created_at: "2021.10.3",
-    updated_at: "2021.10.3",
-  },
-  {
-    id: 2,
-    user_id: 1,
-    title: "Post 2",
-    description: "This is the second post",
-    image_url: "https://bit.ly/2Z4KKcF",
-    image_alt: "image 1",
-    created_at: "2021-05-01T00:00:00.000Z",
-    updated_at: "2021-05-01T00:00:00.000Z",
-  },
-]
 function Timeline() {
-  // paramのidはuser_idです！
-  const { uid } = useParams()
+  const { id } = useParams();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(`http://localhost:3001/users${id}`);
+      setPosts(result.data.posts);
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <div style={{ position: "fixed" }}>
         <Header>
           <Center>
             <Avatar
-              size='md'
+              size="md"
               mr={"16px"}
               src={
                 "https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
@@ -50,9 +39,9 @@ function Timeline() {
       <div>
         <Button
           color={"black"}
-          bg='white'
-          height='170px'
-          width='170px'
+          bg="white"
+          height="170px"
+          width="170px"
           borderRadius={"60px"}
           position={"fixed"}
           marginLeft={"400px"}
@@ -65,21 +54,22 @@ function Timeline() {
           </IconContext.Provider>
         </Button>
         <VStack spacing={"75px"}>
-          {PostItems.map((post) => (
+          {posts.map((post) => (
             <Link
               _hover={{
                 textDecoration: "none",
                 boxShadow: "lg",
                 opacity: "0.95",
               }}
-              to={`/onePostPage/${post.id}`}
+              to={`/onePostPage/${post.post_id}`}
+              key={post.post_id}
             >
-              <Post key={post.id} post={post} />
+              <Post post={post} />
             </Link>
           ))}
         </VStack>
       </div>
     </>
-  )
+  );
 }
-export default Timeline
+export default Timeline;
