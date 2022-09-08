e03d2dda298c56abc39504ac8698b625f3f4f543
AlMikan <nekomimi987@gmail.com>
2022-09-09 00:30:32 +0900

    change modal font

--- web/src/index.js
+++ web/src/index.js
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
+import "@fontsource/m-plus-rounded-1c"; 
+import theme from './theme'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <ChakraProvider>
+  <React.StrictMode >
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
---
+++ web/src/theme.js
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react'
+import "@fontsource/m-plus-rounded-1c"; 
+
+const theme = extendTheme({
+  fonts: {
+    body: `'M PLUS Rounded 1c', sans-serif`,
+  },
+})
+
+export default theme
