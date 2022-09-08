d38d61fc29f2e1dc05aaee501846931b813b1073
Mu-munn <rpgmukai333@gmail.com>
2022-09-08 17:37:43 +0900

    Navbarにuserデータを流せるように

--- web/src/components/Navbar.js
+++ web/src/components/Navbar.js
@@ -1,64 +1,50 @@
-import {
-  Avatar,
-  Box,
-  Flex,
-  Icon,
-  Heading,
-  VStack,
-  Center,
-  Img,
-  Button,
-} from "@chakra-ui/react";
-import { TbHome, TbBookmark } from "react-icons/tb";
-import Link from "./Link";
-import { useAuth } from "../context/AuthContext";
+import { Avatar, Box, Flex, Icon, Heading, VStack, Center, Img, Button } from "@chakra-ui/react"
+import { TbHome, TbBookmark } from "react-icons/tb"
+import Link from "./Link"
+import { useAuth } from "../context/AuthContext"
+import { useEffect, useState } from "react"
 
 const LinkItems = [
   { name: "Home", icon: TbHome, to: "/" },
   { name: "Settings", icon: TbBookmark, to: "/timeline" },
-];
+]
 
 export default function Navbar({ children }) {
-  const user = useAuth();
-  console.log(user);
-
   return (
     <Box
-      minH="100vh"
-      bg="#F8F8F8"
+      minH='100vh'
+      bg='#F8F8F8'
       bgImage={"/bg_image.png"}
       bgRepeat={"no-repeat"}
       bgSize={"cover"}
     >
+      {/* {console.log(user)} */}
       <SidebarContent />
-      <Box ml="134px" p={"10"}>
+      <Box ml='134px' p={"10"}>
         {children}
       </Box>
     </Box>
-  );
+  )
 }
 
 const SidebarContent = ({ onClose, ...rest }) => {
+  const user = useAuth()
   return (
     <Flex
-      bg="#FFFFFF"
-      pos="fixed"
-      w="134px"
-      h="full"
+      bg='#FFFFFF'
+      pos='fixed'
+      w='134px'
+      h='full'
       // py="10"
-      flexDirection="column"
-      justifyContent="space-between"
-      alignItems="center"
+      flexDirection='column'
+      justifyContent='space-between'
+      alignItems='center'
       {...rest}
     >
+      {console.log(user)}
       <Box w={"full"}>
-        <Heading
-          fontSize="xl"
-          fontFamily="monospace"
-          fontWeight="bold"
-          mb={"80px"}
-        >
-          <Img src="/logo.png" />
+        <Heading fontSize='xl' fontFamily='monospace' fontWeight='bold' mb={"80px"}>
+          <Img src='/logo.png' />
         </Heading>
         <VStack spacing={"7"}>
           {LinkItems.map((link) => (
@@ -66,28 +52,32 @@ const SidebarContent = ({ onClose, ...rest }) => {
           ))}
         </VStack>
       </Box>
-      <Box as="button" borderRadius={"100px"} marginBottom={"60px"}>
-        <Img
-          borderRadius={"100px"}
-          width={"60px"}
-          src={
-            "http://flat-icon-design.com/f/f_object_151/s256_f_object_151_0bg.png"
-          }
-        />
+      <Box as='button' borderRadius={"100px"} marginBottom={"60px"}>
+        {user != undefined ? (
+          <Img
+            borderRadius={"100px"}
+            width={"60px"}
+            // src={"http://flat-icon-design.com/f/f_object_151/s256_f_object_151_0bg.png"}
+            // src={user === null ? user.image_url : ""}
+            src={user.data.user_data.image_url}
+          />
+        ) : (
+          <></>
+        )}
       </Box>
     </Flex>
-  );
-};
+  )
+}
 
 const NavItem = ({ icon, to }) => {
   return (
     <Link
       to={to}
-      w="full"
+      w='full'
       h={"50px"}
-      href="#"
+      href='#'
       style={{ textDecoration: "none" }}
-      cursor="pointer"
+      cursor='pointer'
       _focus={{ boxShadow: "none" }}
       _hover={{
         borderRight: "2px solid #F1873B",
@@ -97,7 +87,7 @@ const NavItem = ({ icon, to }) => {
       <Center h={"full"}>
         {icon && (
           <Icon
-            fontSize="27px"
+            fontSize='27px'
             // _groupHover={{
             //   color: "white",
             // }}
@@ -106,5 +96,5 @@ const NavItem = ({ icon, to }) => {
         )}
       </Center>
     </Link>
-  );
-};
+  )
+}
--- web/src/context/AuthContext.js
+++ web/src/context/AuthContext.js
@@ -1,18 +1,18 @@
-import { createContext, useContext, useReducer, useEffect } from "react";
-import axios from "axios";
+import { createContext, useContext, useReducer, useEffect, useState } from "react"
+import axios from "axios"
 // import authApi from "../api/auth";
 
-const AuthContext = createContext();
-const AuthDispatchContext = createContext();
+const AuthContext = createContext()
+const AuthDispatchContext = createContext()
 
 const authReducer = (user, action) => {
   switch (action.type) {
     case "auth/login":
-      return action.user;
+      return action.user
     case "auth/logout":
-      return null;
+      return null
     default:
-      return user;
+      return user
     // case "auth/init":
     //   return action.user;
     // case "auth/add":
@@ -24,19 +24,24 @@ const authReducer = (user, action) => {
     // case "auth/update":
     //   return action.user;
   }
-};
+}
 
 const AuthProvider = ({ children }) => {
-  const [user, dispatch] = useReducer(authReducer, null);
+  const [user, dispatch] = useReducer(authReducer, null)
+  const [testUser, setTestUser] = useState()
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get("http://localhost:3001/login");
-      dispatch({ type: "auth/login", user: result.data.user_data });
-    };
-    fetchData();
-  }, []);
-  console.log(result.data);
+      const result = await axios.get("http://localhost:3001/login")
+      // axios.get("http://localhost:3001/login").then((res) => {
+      //   setTestUser(res)
+      // })
+      setTestUser(await axios.get("http://localhost:3001/login"))
+      // dispatch({ type: "auth/login", user: result.data.user_data })
+    }
+    fetchData()
+  }, [])
+
   // useEffect(() => {
   //   authApi.get().then((user) => {
   //     dispatch({ type: "auth/init", user });
@@ -44,15 +49,13 @@ const AuthProvider = ({ children }) => {
   // }, []);
 
   return (
-    <AuthContext.Provider value={user}>
-      <AuthDispatchContext.Provider value={dispatch}>
-        {children}
-      </AuthDispatchContext.Provider>
+    <AuthContext.Provider value={testUser}>
+      <AuthDispatchContext.Provider value={testUser}>{children}</AuthDispatchContext.Provider>
     </AuthContext.Provider>
-  );
-};
+  )
+}
 
-const useAuth = () => useContext(AuthContext);
-const useDispatchAuth = () => useContext(AuthDispatchContext);
+const useAuth = () => useContext(AuthContext)
+const useDispatchAuth = () => useContext(AuthDispatchContext)
 
-export { useAuth, useDispatchAuth, AuthProvider };
+export { useAuth, useDispatchAuth, AuthProvider }
